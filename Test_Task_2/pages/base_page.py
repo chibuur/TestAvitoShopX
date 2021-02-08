11c81524bc5dc0087d41d8613965b65bf4ed6261
Andrey Shkabura <43777717+chibuur@users.noreply.github.com>
2021-02-08 23:56:51 +0300

    Update base_page.py

--- Test_Task_2/pages/base_page.py
+++ Test_Task_2/pages/base_page.py
@@ -14,14 +14,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
-
+        
+# если требуется проверить наличие элемента
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
         return True
-
+ # если треуется проверить отсутствие элемента
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
